07a1957a3dfd576aa752437a55e04a073e2b4f1a
Arman Parastaran <arman.parastaran@me.com>
2019-06-23 21:16:57 -0400

    Updates

---
+++ precache-manifest.b2a82a6e38711940a275750e3fa89c71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310466b7c64fa74976e2b5b06b9f55fc",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "3daceeef068430aabfb6",
+    "url": "/react/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "60791855cd27849d7877",
+    "url": "/react/static/js/2.2f19f017.chunk.js"
+  },
+  {
+    "revision": "3daceeef068430aabfb6",
+    "url": "/react/static/js/main.06552a15.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  }
+]);
